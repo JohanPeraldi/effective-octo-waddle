d0a20782ad4637e3aeb8bccaed0bd09cab4aaebd
Johan Peraldi <johan.peraldi@gmail.com>
2024-03-17 17:30:59 +0200

    Switch config environment variable to "production"

--- config.php
+++ config.php
@@ -1,5 +1,3 @@
 <?php
-// Comment one or the other of the following lines
-// depending on which environment you are working on
-$environment = 'development';
-// $environment = 'production';
+
+$environment = 'production';
